37c34930e95fc5c944175cf2c9c0601a757efc9a
Matt Gibb <matt@mattgibb.com>
2014-01-18 21:41:51 -0800

    Remote db update

--- bin/import.sh
+++ bin/import.sh
@@ -4,10 +4,26 @@ set -e
 curl 'https://www.lendingclub.com/fileDownload.action?file=InFundingStats3.csv.zip&type=gen' > file.zip
 unzip -cqq file.zip | awk 'NR != 1 { print }' > csvs/latest.csv
 cp csvs/{latest,$(date +"%T")}.csv
-mongoimport --db meteor\
+
+# local
+# mongoimport --db meteor\
+#             --collection loans\
+#             --host 127.0.0.1\
+#             --port 3002\
+#             --type csv\
+#             --file csvs/latest.csv\
+#             --headerline\
+#             --upsert\
+#             --stopOnError\
+#             --upsertFields id
+
+# mongohq
+mongoimport --db ediveX9i\
+            --username crowdminr\
+            --password chowno1\
             --collection loans\
-            --host 127.0.0.1\
-            --port 3002\
+            --host mongo.onmodulus.net\
+            --port 27017\
             --type csv\
             --file csvs/latest.csv\
             --headerline\
